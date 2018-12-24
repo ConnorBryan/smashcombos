901879a666878fb9e0bd794973eb010e713e3167
Connor Bryan <cchromium@gmail.com>
2018-12-24 16:13:31 -0600

    ...

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,70 +1,71 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: "SmashCombos",
+    description:
+      "Attributes, kill confirms and combos for every character in the Super Smash Bros. Ultimate cast."
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads',
-      },
+        name: "uploads"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+        name: "pages"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+        name: "images"
+      }
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-relative-images',
+            resolve: "gatsby-remark-relative-images",
             options: {
-              name: 'uploads',
-            },
+              name: "uploads"
+            }
           },
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
+              maxWidth: 2048
+            }
           },
           {
-            resolve: 'gatsby-remark-copy-linked-files',
+            resolve: "gatsby-remark-copy-linked-files",
             options: {
-              destinationDir: 'static',
+              destinationDir: "static"
             }
           }
-        ],
-      },
+        ]
+      }
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
     },
-    'gatsby-plugin-purgecss', // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
-}
+    "gatsby-plugin-purgecss", // must be after other CSS plugins
+    "gatsby-plugin-netlify" // make sure to keep it last in the array
+  ]
+};
---
+++ src/pages/characters/pikachu.md
@@ -0,0 +1,34 @@
+---
+name: Pikachu
+templateKey: "character-page"
+render: /img/pikachu.png
+attributes:
+  weight:
+    weightClass: featherweight
+    weightRank: "68"
+    weightValue: "79"
+killConfirms:
+  balloonweightPercentage: "160"
+  featherweightPercentage: "160"
+  heavyweightPercentage: "160"
+  input: uthrow
+  lightweightPercentage: "160"
+  middleweightPercentage: "160"
+  superHeavyweightPercentage: "160"
+combos:
+  - balloonweightPercentage: "0"
+    featherweightPercentage: "0"
+    heavyweightPercentage: "0"
+    input: dthrow nair
+    lightweightPercentage: "0"
+    middleweightPercentage: "0"
+    superHeavyweightPercentage: "0"
+  - balloonweightPercentage: "75"
+    featherweightPercentage: "75"
+    heavyweightPercentage: "75"
+    input: uthrow dspecial
+    killConfirm: true
+    lightweightPercentage: "75"
+    middleweightPercentage: "75"
+    superHeavyweightPercentage: "75"
+---
