c6df11d35fbd45ad092c4427a1395fca866e53be
Connor Bryan <cchromium@gmail.com>
2019-01-09 15:38:04 -0600

    Add KuroganeHammer ad

--- src/components/attributes.js
+++ src/components/attributes.js
@@ -1,34 +1,43 @@
 import React, { Component } from "react";
 import {
+  Button,
+  Card,
   Dropdown,
+  Grid,
   Header,
   Icon,
-  Label,
+  Image,
   List,
   Segment,
   Tab
 } from "semantic-ui-react";
 
 import { AttributeTypes, attributeToInformationHash } from "../helpers";
+import kuroganeHammer from "../img/kurogane-hammer.png";
 import * as styles from "../styles";
 
 export default class Attributes extends Component {
   static Box({ attribute, header, rank, fields }) {
     return (
-      <React.Fragment>
-        <Header as="h3" style={styles.fancyText} content={header} />
-        <Label
-          ribbon="right"
-          content={
-            <span>
-              <Icon name="trophy" /> Rank {rank} of 76
-            </span>
-          }
+      <Segment basic>
+        <Header
+          as="h3"
+          style={{
+            ...styles.fancyText,
+            marginBottom: 0
+          }}
+          content={header}
         />
-        <List divided size="large">
-          {fields.map(({ label, value }) => (
+        <List
+          divided
+          size="large"
+          style={{
+            height: "187px"
+          }}
+        >
+          {fields.map(({ label, value }, index) => (
             <List.Item
-              key={label}
+              key={index}
               style={{
                 flex: 1,
                 display: "flex",
@@ -45,8 +54,30 @@ export default class Attributes extends Component {
               />
             </List.Item>
           ))}
+          <List.Item
+            style={{
+              flex: 1,
+              display: "flex",
+              padding: "1rem 0"
+            }}
+          >
+            <List.Header
+              content={
+                <React.Fragment>
+                  <Icon name="trophy" /> Rank
+                </React.Fragment>
+              }
+            />
+            <List.Content
+              content={`${rank} of 76`}
+              style={{
+                flex: 1,
+                textAlign: "right"
+              }}
+            />
+          </List.Item>
         </List>
-      </React.Fragment>
+      </Segment>
     );
   }
 
@@ -62,69 +93,107 @@ export default class Attributes extends Component {
 
     return (
       <Segment basic>
-        <Header
-          as="h2"
-          style={{
-            ...styles.fancyText,
-            marginBottom: "2rem",
-            color: "#7289D8"
-          }}
-        >
-          Attributes
-        </Header>
-        <div className="mobile-only">
-          <Dropdown
-            fluid
-            attached="top"
-            selection
-            value={attribute}
-            onChange={this.switchAttribute}
-            options={Object.entries(attributeToInformationHash).map(
-              ([key, { name, value }]) => ({
-                key,
-                text: name,
-                value
-              })
-            )}
-          />
-          <Segment attached="bottom">
-            <Attributes.Box
-              attribute={attributes[attribute]}
-              header={attributeToInformationHash[attribute].name}
-              rank={attributes[attribute].rank}
-              fields={attributeToInformationHash[attribute].fields}
-            />
-          </Segment>
-        </div>
-        <div className="desktop-only">
-          <Tab
-            className="attributes"
-            menu={{
-              fluid: true,
-              vertical: true,
-              tabular: true
-            }}
-            panes={Object.entries(attributeToInformationHash).map(
-              ([key, { name, value: type, fields }]) => ({
-                menuItem: name,
-                render: () => (
-                  <Tab.Pane>
-                    <Attributes.Box
-                      attribute={attributes[type]}
-                      header={name}
-                      rank={attributes[type].rank}
-                      fields={fields}
-                    />
-                  </Tab.Pane>
-                )
-              })
-            )}
-            style={{
-              maxWidth: "700px",
-              marginBottom: "3rem"
-            }}
-          />
-        </div>
+        <Grid>
+          <Grid.Column mobile={16} tablet={9} computer={9}>
+            <Header
+              as="h2"
+              style={{
+                ...styles.fancyText,
+                marginBottom: "2rem",
+                color: "#7289D8"
+              }}
+            >
+              Attributes
+            </Header>
+            <div className="mobile-only">
+              <Dropdown
+                fluid
+                attached="top"
+                selection
+                value={attribute}
+                onChange={this.switchAttribute}
+                options={Object.entries(attributeToInformationHash).map(
+                  ([key, { name, value }]) => ({
+                    key,
+                    text: name,
+                    value
+                  })
+                )}
+              />
+              <Segment attached="bottom">
+                <Attributes.Box
+                  attribute={attributes[attribute]}
+                  header={attributeToInformationHash[attribute].name}
+                  rank={attributes[attribute].rank}
+                  fields={attributeToInformationHash[attribute].fields}
+                />
+              </Segment>
+            </div>
+            <div className="desktop-only">
+              <Tab
+                className="attributes"
+                menu={{
+                  fluid: true,
+                  vertical: true,
+                  tabular: true
+                }}
+                panes={Object.entries(attributeToInformationHash).map(
+                  ([key, { name, value: type, fields }]) => ({
+                    menuItem: name,
+                    render: () => (
+                      <Tab.Pane>
+                        <Attributes.Box
+                          attribute={attributes[type]}
+                          header={name}
+                          rank={attributes[type].rank}
+                          fields={fields}
+                        />
+                      </Tab.Pane>
+                    )
+                  })
+                )}
+                style={{
+                  marginBottom: "3rem"
+                }}
+              />
+            </div>
+          </Grid.Column>
+          <Grid.Column
+            mobile={16}
+            tablet={7}
+            computer={7}
+            verticalAlign="middle"
+          >
+            <Card
+              fluid
+              style={{
+                background: "#eee"
+              }}
+            >
+              <Image src={kuroganeHammer} alt="Kurogane Hammer logo" />
+              <Card.Content
+                content="Character attributes sourced from KuroganeHammer, with permission."
+                textAlign="center"
+              />
+              <Card.Content
+                extra
+                style={{
+                  padding: 0
+                }}
+              >
+                <Button
+                  primary
+                  fluid
+                  as="a"
+                  href="http://kuroganehammer.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  content="Check them out"
+                />
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid>
       </Segment>
     );
   }
