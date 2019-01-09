292f2de26e0d35c4fd2edc9d985296e61b177251
Connor Bryan <cchromium@gmail.com>
2019-01-09 11:13:28 -0600

    Fix mobile menu scrolling

--- src/components/menu-provider.js
+++ src/components/menu-provider.js
@@ -5,12 +5,19 @@ export const MenuContext = React.createContext();
 export default class MenuProvider extends Component {
   state = {
     isOpen: false,
-    open: () => this.setState({ isOpen: true }),
-    close: () => this.setState({ isOpen: false }),
-    toggle: () =>
-      this.setState(prevState => ({
-        isOpen: !prevState.isOpen
-      }))
+    open: () => {
+      this.setState({ isOpen: true });
+      document.body.classList.add("hide-scroll");
+    },
+    close: () => {
+      this.setState({ isOpen: false });
+      document.body.classList.remove("hide-scroll");
+    },
+    toggle: () => {
+      const { isOpen } = this.state;
+
+      isOpen ? this.state.close() : this.state.open();
+    }
   };
 
   render() {
