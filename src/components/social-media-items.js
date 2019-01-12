bb7d3ac6bbd82f16fdab608ebf003b5dda721c31
Connor Bryan <cchromium@gmail.com>
2019-01-11 23:15:53 -0600

    Add color highlights for social media buttons

--- src/components/social-media-items.js
+++ src/components/social-media-items.js
@@ -5,8 +5,16 @@ import { SOCIAL_MEDIA } from "../config";
 import * as styles from "../styles";
 
 export default function SocialMediaItems() {
-  return SOCIAL_MEDIA.map(({ site, url, tagline }) => (
-    <Menu.Item key={site} as="a" href={url} style={styles.fancyText}>
+  return SOCIAL_MEDIA.map(({ site, url, color, tagline }) => (
+    <Menu.Item
+      key={site}
+      as="a"
+      href={url}
+      style={{
+        ...styles.fancyText,
+        borderLeft: `2px solid ${color}`
+      }}
+    >
       <Icon name={site} /> {tagline}
     </Menu.Item>
   ));
