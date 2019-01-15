47ef271f9ac450ae1faa014a6af923d57afb97ec
Connor Bryan <cchromium@gmail.com>
2019-01-14 18:36:07 -0600

    PWA?

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,15 +2,4 @@ const netlifyIdentity = require("netlify-identity-widget");
 
 exports.onClientEntry = () => {
   netlifyIdentity.init();
-
-  let deferredPrompt;
-
-  window.addEventListener("beforeinstallprompt", e => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-
-    console.log("Before install prompt.");
-  });
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
+import { Button, Icon, Item } from "semantic-ui-react";
 
 import { CharacterSelect, Layout } from "../components";
+import smashball from "../img/smashball.png";
+import * as styles from "../styles";
 
-export default function IndexPage() {
-  return (
-    <Layout>
-      <CharacterSelect />
-    </Layout>
-  );
+export default class IndexPage extends Component {
+  installAppPrompt = null;
+
+  componentDidMount() {
+    // Progressive Web App
+    window.addEventListener("beforeinstallprompt", e => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.installAppPrompt = e;
+    });
+  }
+
+  attemptToInstallApp = () => {
+    if (this.installAppPrompt) {
+      this.installAppPrompt.prompt();
+    }
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Item.Group
+          relaxed="very"
+          style={{
+            marginBottom: "8rem"
+          }}
+        >
+          <Item>
+            <Item.Image size="medium" src={smashball} />
+            <Item.Content verticalAlign="middle">
+              <Item.Header
+                as="h1"
+                style={{
+                  ...styles.fancyText,
+                  fontSize: "2rem"
+                }}
+                content="Attributes and combos for all members of the Smash Ultimate cast"
+              />
+              <Item.Description
+                style={{
+                  fontSize: "1.2rem"
+                }}
+              >
+                SmashCombos is an open-source initiative with the goal of
+                becoming a compendium of combos, a one-stop-shop to pick up the
+                ins and outs of your favorite character, or even a character
+                you're looking to add to your repertoire.
+              </Item.Description>
+              <Item.Extra>
+                <Button
+                  icon
+                  primary
+                  size="massive"
+                  floated="right"
+                  onClick={this.attemptToInstallApp}
+                  style={{
+                    marginTop: "2rem"
+                  }}
+                >
+                  <Icon name="mobile" /> Download the App
+                </Button>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+        <CharacterSelect />
+      </Layout>
+    );
+  }
 }
