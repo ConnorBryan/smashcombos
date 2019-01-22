e225558046f26d671aaa1210db93577a4b98dd4f
Connor Bryan <cchromium@gmail.com>
2019-01-22 16:48:27 -0600

    Pass user to each API call

--- src/services/CharacterService.js
+++ src/services/CharacterService.js
@@ -1,12 +1,22 @@
 import axios from "axios";
 import uuid from "uuid/v4";
 
-const URL_BASE = "https://smashcombos.xyz";
+import { auth } from "../components/user-provider";
+
+// const URL_BASE = "https://smashcombos.xyz";
+const URL_BASE = "http://localhost:1337";
 
 export class CharacterService {
   static async editProfile(name, description, tags) {
+    const user = auth.currentUser();
+
+    if (!user) {
+      throw new Error(`Unable to edit a profile when not signed in.`);
+    }
+
     const url = `${URL_BASE}/characters/${name}/profile`;
     const { data } = await axios.post(url, {
+      user,
       description,
       tags
     });
@@ -15,8 +25,15 @@ export class CharacterService {
   }
 
   static async addCombo(name, combo) {
+    const user = auth.currentUser();
+
+    if (!user) {
+      throw new Error(`Unable to edit a profile when not signed in.`);
+    }
+
     const url = `${URL_BASE}/characters/${name}/combos`;
     const { data } = await axios.post(url, {
+      user,
       combo: {
         ...combo,
         uuid: uuid()
@@ -27,8 +44,15 @@ export class CharacterService {
   }
 
   static async editCombo(name, uuid, combo) {
+    const user = auth.currentUser();
+
+    if (!user) {
+      throw new Error(`Unable to edit a profile when not signed in.`);
+    }
+
     const url = `${URL_BASE}/characters/${name}/combos/${uuid}`;
     const { data } = await axios.post(url, {
+      user,
       combo
     });
 
