ca3d125b604d9a567c158c08f17518c6006cd637
Connor Bryan <cchromium@gmail.com>
2019-01-22 18:31:57 -0600

    ...

--- src/modules/Dashboard/Dashboard.js
+++ src/modules/Dashboard/Dashboard.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-import { UserService } from "../../services";
+import { auth, UserService } from "../../services";
 
 export default function Dashboard({ user }) {
+  console.log("\n\n\n", "auth.currentUser()", auth.currentUser(), "\n\n\n");
+
   return (
     <div>
       <button onClick={UserService.updateUsername}>Derp</button>
